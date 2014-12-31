eaa0759ea06bab53487d10c892b822fb1ea18752
davidsbatista <dsbatista@gmail.com>
2014-12-31 01:41:48 +0000

    initial commit

--- malt-parser-example.py
+++ malt-parser-example.py
@@ -2,29 +2,34 @@
 # -*- coding: utf-8 -*-
 
 from nltk import TreebankWordTokenizer, DependencyGraph
-from nltk.parse import MaltParser
+#from nltk.parse import MaltParser
+
+from malt import MaltParser
 
 __author__ = 'dsbatista'
 
 
 def main():
-    parser = MaltParser(working_dir="/home/dsbatista/maltparser-1.8", mco="dep-parser-model1.mco",
+    parser = MaltParser(working_dir="/home/dsbatista/maltparser-1.8", mco="dep-parser-model3",
                         additional_java_args=['-Xmx512m'])
 
     txt = "A explosao, que ocorreu pelas 12h00, destruiu quase por completo um edificio de quatro andares do " \
           "complexo da fabrica Stockline Plastics, localizada no centro de Glasgow."
 
-    sentence = TreebankWordTokenizer().tokenize(txt)
+    txt = "O PS apoiou a candidatura de Rui Soares."
 
-    print sentence
-
-    graph = parser.parse(sentence, verbose=True)
-
-    g = DependencyGraph()
-
-    graph.tree().draw()
+    graph = parser.raw_parse(txt)
     graph.tree().pprint()
 
+    sentence = TreebankWordTokenizer().tokenize(txt)
+    print sentence
+    graph = parser.parse(sentence, verbose=True)
+    #print type(graph)
+    #print dir(graph)
+    print graph.tree()
+    print graph.to_conll(10)
+    #graph.tree().pprint()
+    #graph.tree().draw()
 
 if __name__ == "__main__":
     main()
