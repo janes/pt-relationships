6fad7938766fbb81da901f6713a3d9ee23109bf5
David S. Batista <dsbatista@gmail.com>
2017-02-05 21:03:40 +0100

    finds begin and end position of entities in syntaxnet info

--- process_sentences.py
+++ process_sentences.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import pickle
+import os
+import re
 import subprocess
 import codecs
 import sys
-import re
+
+from nltk.stem.wordnet import WordNetLemmatizer
 
 __author__ = "David S. Batista"
 __email__ = "dsbatista@gmail.com"
@@ -15,11 +19,19 @@ regex = re.compile('<[A-Z]+>[^<]+</[A-Z]+>', re.U)
 
 class Relationship:
 
-    def __init__(self, sentence, ent1, ent2, rel_type):
+    def __init__(self, sentence, ent1, ent2, rel_type, syntaxnet_info=None):
         self.sentence = sentence
         self.rel_type = rel_type
         self.ent1 = ent1
         self.ent2 = ent2
+        self.syntaxnet_info = syntaxnet_info
+        self.ent1_begin = None
+        self.ent1_end = None
+        self.ent2_begin = None
+        self.ent2_end = None
+        self.bef = None
+        self.bet = None
+        self.aft = None
 
 
 def load_relationships(data_file):
@@ -52,23 +64,111 @@ def load_relationships(data_file):
 
 
 def extract_features(rel):
-    pass
+    """
+
+    BETWEEN:
+        - the words
+        - parts of speech
+
+        - prefixes of length 5
+        - words between the nominals can be strong indicators for the
+        type of relation, using the prefixes of length 5 for the words
+        between the nominals provides a kind of stemming
+
+        - extract a coarse-grained part of speech sequence for the words
+        between the nominals; a string using the first letter of each
+        token’s Treebank POS tag. This feature is motivated by the fact that
+        relations such as Member-Collection usually invoke prepositional
+        phrases such as:  of, in the, and of various.
+        The corresponding POS sequences we extract are: “I”, “I D”, and “I J”.
+
+        - Finally, we also use the number of words between the nominals as a
+        feature because relations such as Product-Producer and Entity-Origin
+        often have no intervening tokens (e.g., organ builder or Coconut oil).
+
+    BEFORE:
+        - the words before and single word after E1 and E2 respectively
+
+    """
+
+
+def syntactic_path(sentence):
+    """
+    1  ID: Word index, integer starting at 1 for each new sentence; may be a range for multiword tokens; may be a decimal number for empty nodes.
+    2  FORM: Word form or punctuation symbol.
+    3  LEMMA: Lemma or stem of word form.
+    4  UPOSTAG: Universal part-of-speech tag.
+    5  XPOSTAG: Language-specific part-of-speech tag; underscore if not available.
+    6  FEATS: List of morphological features from the universal feature inventory or from a defined language-specific extension; underscore if not available.
+    7  HEAD: Head of the current word, which is either a value of ID or zero (0).
+    8  DEPREL: Universal dependency relation to the HEAD (root iff HEAD = 0) or a defined language-specific subtype of one.
+    9  DEPS: Enhanced dependency graph in the form of a list of head-deprel pairs.
+    10 MISC: Any other annotation.
+
+    :param sentence:
+    :return:
+    """
+
+
+def get_contexts(rel):
+
+    print rel.sentence
+
+    ent1_parts = get_entity_parts(rel.ent1)
+    ent2_parts = get_entity_parts(rel.ent2)
+
+    rel.ent1_begin, rel.ent1_end = get_entity_position(ent1_parts, rel)
+    rel.ent2_begin, rel.ent2_end = get_entity_position(ent2_parts, rel)
+
+    print
+    print rel.syntaxnet_info[rel.ent1_begin]
+    print rel.syntaxnet_info[rel.ent1_end]
+    print
+    print rel.syntaxnet_info[rel.ent2_begin]
+    print rel.syntaxnet_info[rel.ent2_end]
+
+
+def get_entity_position(ent_parts, rel):
+
+    z = 0
+    begin = 0
+    end = 0
+
+    for i in range(len(rel.syntaxnet_info)):
+        if rel.syntaxnet_info[i][1].strip(",") == ent_parts[z].encode("utf8"):
+
+            # entity is a single token
+            if z == 0 and len(ent_parts) == 1:
+                begin = end = i
+                break
+
+            if z == 0:
+                begin = i
+                z += 1
+
+            elif z == len(ent_parts)-1:
+                end = i
+                break
+
+            else:
+                z += 1
+
+    return begin, end
+
+
+def get_entity_parts(entity):
+    clean = re.sub("</?[A-Z]+>", "", entity)
+    entity_parts = clean.split()
+    return entity_parts
 
 
 def read_syntaxnet_output(sentences):
 
-    #print len(sentences)
-    #exit(-1)
+    # joint all sentences into a single string
+    all_sentences = "\n".join(sentences)
 
-    all_sentences = "\n".join(sentences[0:1100])
-
-    process = subprocess.Popen(
-        """echo '%s'""" % all_sentences,
-        shell=True,
-        universal_newlines=False,
-        stdout=subprocess.PIPE)
-
-    output = process.communicate()
+    # redirect std_error to /dev/null
+    FNULL = open(os.devnull, 'w')
 
     process = subprocess.Popen(
         'MODEL_DIRECTORY=/Users/dbatista/Downloads/Portuguese; '
@@ -77,7 +177,8 @@ def read_syntaxnet_output(sentences):
         '$MODEL_DIRECTORY 2' % all_sentences,
         shell=True,
         universal_newlines=False,
-        stdout=subprocess.PIPE)
+        stdout=subprocess.PIPE,
+        stderr=FNULL)
 
     output = process.communicate()
     processed_sentences = []
@@ -98,6 +199,9 @@ def read_syntaxnet_output(sentences):
 
     print len(processed_sentences)
 
+    f_out = open("processed_sentences.pkl", "w")
+    pickle.dump(processed_sentences, f_out)
+    f_out.close()
 
     """
     # find ROOT verb
@@ -109,11 +213,34 @@ def read_syntaxnet_output(sentences):
     """
 
 
+def lemmatize():
+    lmtzr = WordNetLemmatizer()
+    lmtzr.lemmatize("owns", "v")
+    lmtzr.lemmatize("owned", "v")
+
+
 def main():
     relationships = load_relationships(sys.argv[1])
-    sentences = list(set([re.sub(r"</?[A-Z]+>", "", x.sentence.strip())
-                          for x in relationships]))
+
+    """
+    sentences = [re.sub(r"</?[A-Z]+>", "", x.sentence.strip())
+                 for x in relationships]
     read_syntaxnet_output(sentences)
+    """
+
+    f_out = open("processed_sentences.pkl", "r")
+    sentences_processed = pickle.load(f_out)
+
+    for x in range(0, len(relationships)):
+        relationships[x].syntaxnet_info = sentences_processed[x]
+
+    get_contexts(relationships[int(sys.argv[2])])
+
+    # TODO: para visualizar a dep-tree procurar um CONLL2GraphTree
+    """
+    for word_info in rel.syntaxnet_info:
+        print word_info[0], word_info[1], word_info[3], word_info[6], word_info[7]
+    """
 
 if __name__ == "__main__":
     main()
